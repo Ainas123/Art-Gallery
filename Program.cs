19cb4a9a7028c5f2d47ea107757ec7383fd2dc35
Ainas123 <113600828+Ainas123@users.noreply.github.com>
2025-04-23 18:48:22 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Art_Gallery.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<artGallery>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("myconnection")));
+
+builder.Services.AddSession(option => {
+ option.IdleTimeout = TimeSpan.FromMinutes(10);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=User}/{action=Index}/{id?}");
+
+app.Run();
